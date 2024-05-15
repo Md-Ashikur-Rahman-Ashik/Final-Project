fe6a1cc58ce77e3740d62ee952c53825f1ca832a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-15 20:52:34 +0600

    Routes, layout and footer setup completed

---
+++ project-client/src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Footer/Footer";
+import NavBar from "../pages/NavBar/NavBar";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
