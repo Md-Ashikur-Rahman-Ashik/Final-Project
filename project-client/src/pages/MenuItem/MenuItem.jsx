41cd8fa51e5ee20af95d033c881636697c8b1481
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-16 13:00:19 +0600

    Displayed popular items in the home page

---
+++ project-client/src/pages/MenuItem/MenuItem.jsx
@@ -0,0 +1,21 @@
+const MenuItem = ({ item }) => {
+  const { image, price, name, recipe } = item;
+
+  return (
+    <div className="flex space-x-2">
+      <img
+        src={image}
+        className="w-[100px]"
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        alt=""
+      />
+      <div>
+        <h3 className="uppercase">{name}-----</h3>
+        <p>{recipe}</p>
+      </div>
+      <p className="text-yellow-500">${price}</p>
+    </div>
+  );
+};
+
+export default MenuItem;
