7f36a0554c6ef89a1aac6ed4969b11f7bf70da12
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-19 10:25:41 +0600

    Displayed different menu category items with cover

--- project-client/src/pages/Menu/Menu.jsx
+++ project-client/src/pages/Menu/Menu.jsx
@@ -1,14 +1,56 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Cover/Cover";
 import menuImg from "../../assets/menu/banner3.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg"
+import useMenu from "../../hooks/useMenu";
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import MenuCategory from "./MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soups = menu.filter((item) => item.category === "soup");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImg} title={"our menu"}></Cover>
+      {/* Main cover */}
+      <SectionTitle
+        subHeading={"Don't Miss"}
+        heading={"Today's Offer"}
+      ></SectionTitle>
+      {/* Offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* Dessert menu items */}
+      <MenuCategory
+        items={desserts}
+        img={dessertImg}
+        title={"Dessert"}
+      ></MenuCategory>
+      <MenuCategory
+        items={pizzas}
+        title={"Pizza"}
+        img={pizzaImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={salads}
+        title={"Salad"}
+        img={saladImg}
+      ></MenuCategory>
+      <MenuCategory
+        items={soups}
+        title={"Soup"}
+        img={soupImg}
+      ></MenuCategory>
     </div>
   );
 };
