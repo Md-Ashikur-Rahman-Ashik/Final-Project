4d4bd88d9553441d15e549b252da2ef54f970686
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-22 14:28:48 +0600

    Completed firebase project setup for Bistro Boss

--- project-client/src/Layout/Main.jsx
+++ project-client/src/Layout/Main.jsx
@@ -4,7 +4,7 @@ import NavBar from "../pages/NavBar/NavBar";
 
 const Main = () => {
   const location = useLocation();
-  console.log(location);
+  // console.log(location);
 
   const noHeaderFooter = location.pathname.includes("login");
 
