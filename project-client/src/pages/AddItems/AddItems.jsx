02e381d5cf7f84f8e10a6b54e0b35bae37602788
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-24 21:05:20 +0600

    Designed form using daisy ui and react hook form

--- project-client/src/pages/AddItems/AddItems.jsx
+++ project-client/src/pages/AddItems/AddItems.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
@@ -13,21 +14,71 @@ const AddItems = () => {
       ></SectionTitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} />
-          <select
-            {...register("category")}
-            className="select select-bordered w-full max-w-xs"
-          >
-            <option disabled selected>
-              Select a category
-            </option>
-            <option value="salad">Salad</option>
-            <option value="pizza">Pizza</option>
-            <option value="soup">Soup</option>
-            <option value="dessert">Dessert</option>
-            <option value="drinks">Drinks</option>
-          </select>
-          <input type="submit" />
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Recipe Name*</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Recipe Name"
+              {...register("name", { required: true })}
+              className="input input-bordered w-full"
+            />
+          </label>
+          <div className="flex gap-6">
+            {/* Category */}
+            <label className="form-control w-full my-6">
+              <div className="label">
+                <span className="label-text">Category*</span>
+              </div>
+              <select
+                {...register("category", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled>
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </label>
+
+            {/* Price */}
+            <label className="form-control w-full my-6">
+              <div className="label">
+                <span className="label-text">Price*</span>
+              </div>
+              <input
+                type="number"
+                placeholder="Price"
+                {...register("price", { required: true })}
+                className="input input-bordered w-full"
+              />
+            </label>
+          </div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details</span>
+            </div>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </label>
+          <div className="my-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+          <button className="btn">
+            Add Item <FaUtensils></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
