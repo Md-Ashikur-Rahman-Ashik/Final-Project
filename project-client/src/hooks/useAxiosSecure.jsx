ce7b4397a5ece3f68b4edf281842a8c6bec0c60a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-24 12:27:00 +0600

    Logged out unauthorized access
    Also, checked is Admin

--- project-client/src/hooks/useAxiosSecure.jsx
+++ project-client/src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   // Request interceptor to add authorization header for every secure call to the api
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -23,8 +27,13 @@ const useAxiosSecure = () => {
     function (response) {
       return response;
     },
-    function (error) {
+    async function (error) {
       const status = error.response.status;
+      // For 401 or 403 logout the user and move the user to the login page
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
